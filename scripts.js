ac25bd212f56d3d1e6779c3c04b864d54650dad1
Jorge Antonio Morales Florez <jamf_99@hotmail.com>
2020-02-02 16:16:21 -0500

    Web page completed

---
+++ scripts.js
@@ -0,0 +1,215 @@
+var modal1 = document.getElementById("modal1");
+var text1 = document.getElementById("text1");
+var span1 = document.getElementsByClassName("close1")[0];
+
+var modal2 = document.getElementById("modal2");
+var text2 = document.getElementById("text2");
+var span2 = document.getElementsByClassName("close2")[0];
+
+var modal3 = document.getElementById("modal3");
+var text3 = document.getElementById("text3");
+var span3 = document.getElementsByClassName("close3")[0];
+
+var modal4 = document.getElementById("modal4");
+var text4 = document.getElementById("text4");
+var span4 = document.getElementsByClassName("close4")[0];
+
+var modal5 = document.getElementById("modal5");
+var text5 = document.getElementById("text5");
+var span5 = document.getElementsByClassName("close5")[0];
+
+var modal6 = document.getElementById("modal6");
+var text6 = document.getElementById("text6");
+var span6 = document.getElementsByClassName("close6")[0];
+
+var modal7 = document.getElementById("modal7");
+var text7 = document.getElementById("text7");
+var span7 = document.getElementsByClassName("close7")[0];
+
+var modal8 = document.getElementById("modal8");
+var text8 = document.getElementById("text8");
+var span8 = document.getElementsByClassName("close8")[0];
+
+var modal9 = document.getElementById("modal9");
+var text9 = document.getElementById("text9");
+var span9 = document.getElementsByClassName("close9")[0];
+
+var modal10 = document.getElementById("modal10");
+var text10 = document.getElementById("text10");
+var span10 = document.getElementsByClassName("close10")[0];
+
+var modal11 = document.getElementById("modal11");
+var text11 = document.getElementById("text11");
+var span11 = document.getElementsByClassName("close11")[0];
+
+var modal12 = document.getElementById("modal12");
+var text12 = document.getElementById("text12");
+var span12 = document.getElementsByClassName("close12")[0];
+
+text1.onclick = function() {
+  modal1.style.display = "block";
+}
+
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+
+text2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
+
+text3.onclick = function() {
+  modal2.style.display = "block";
+}
+
+span3.onclick = function() {
+  modal2.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+}
+
+text4.onclick = function() {
+  modal4.style.display = "block";
+}
+
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+}
+
+text5.onclick = function() {
+  modal5.style.display = "block";
+}
+
+span5.onclick = function() {
+  modal5.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal5) {
+    modal5.style.display = "none";
+  }
+}
+
+text6.onclick = function() {
+  modal6.style.display = "block";
+}
+
+span6.onclick = function() {
+  modal6.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal6) {
+    modal6.style.display = "none";
+  }
+}
+
+text7.onclick = function() {
+  modal7.style.display = "block";
+}
+
+span7.onclick = function() {
+  modal7.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal7) {
+    modal7.style.display = "none";
+  }
+}
+
+text8.onclick = function() {
+  modal8.style.display = "block";
+}
+
+span8.onclick = function() {
+  modal8.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal8) {
+    modal8.style.display = "none";
+  }
+}
+
+text9.onclick = function() {
+  modal9.style.display = "block";
+}
+
+span9.onclick = function() {
+  modal9.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal9) {
+    modal9.style.display = "none";
+  }
+}
+
+text10.onclick = function() {
+  modal10.style.display = "block";
+}
+
+span10.onclick = function() {
+  modal10.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal10) {
+    modal10.style.display = "none";
+  }
+}
+
+text11.onclick = function() {
+  modal11.style.display = "block";
+}
+
+span11.onclick = function() {
+  modal11.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal11) {
+    modal11.style.display = "none";
+  }
+}
+
+text12.onclick = function() {
+  modal12.style.display = "block";
+}
+
+span12.onclick = function() {
+  modal12.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal12) {
+    modal12.style.display = "none";
+  }
+}
